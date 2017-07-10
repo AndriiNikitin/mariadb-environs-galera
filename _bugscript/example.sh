66deede8443be9e5acc4bf23348ce5e71feca943
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-07-10 20:49:12 +0200

    fix for travis

--- _bugscript/example.sh
+++ _bugscript/example.sh
@@ -14,14 +14,14 @@ m0*/download.sh
 # workaround MDEV-13283
 sed -i "s/Distrib 10.1/Distrib 10/g" _depot/m-tar/${ver}/bin/wsrep_sst_mysqldump
 
-cluster1/gen_cnf.sh
-cluster1/install_db.sh
-cluster1/galera_setup_acl.sh
-cluster1/galera_start_new.sh $WSREP_EXTRA_OPT
+. cluster1/gen_cnf.sh
+. cluster1/install_db.sh
+. cluster1/galera_setup_acl.sh
+. cluster1/galera_start_new.sh $WSREP_EXTRA_OPT
 
 sleep 45
-cluster1/galera_cluster_size.sh
-cluster1/sql.sh 'show variables like "wsrep_sst_method"'
+. cluster1/galera_cluster_size.sh
+. cluster1/sql.sh 'show variables like "wsrep_sst_method"'
 
 grep -A10 -B10 -i "\[ERROR\]" m0*/dt/error.log || echo no errors found
 
