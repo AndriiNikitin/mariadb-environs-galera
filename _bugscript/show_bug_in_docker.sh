509e298292b4fad9010253902a3626b5500e7f82
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-07-10 16:39:05 +0200

    test sst methods

--- _bugscript/show_bug_in_docker.sh
+++ _bugscript/show_bug_in_docker.sh
@@ -5,13 +5,14 @@ from centos:7
 
 RUN yum -y install m4 git wget python cmake make gcc-c++ ncurses-devel bison zlib zlib-devel zlib-static openssl vim findutils openssl vim m4 libaio libnuma numactl gnutls-devel openssl098e
 
-COPY ${1}.sh ${1}.sh
+COPY ${1:example}.sh ${1:example}.sh
 
-RUN cat ${1}.sh
+RUN cat ${1:example}.sh
 
 ENV WORKAROUND "$WORKAROUND"
+ENV WSREP_EXTRA_OPT "$WSREP_EXTRA_OPT"
 
-RUN bash -v -x ${1}.sh
+RUN bash -v -x ${1:example}.sh
 EOF
 
 docker build .
