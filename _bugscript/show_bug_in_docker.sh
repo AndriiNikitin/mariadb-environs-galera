bb1190a4f2e568707c4f94e3217fb63035adec54
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-07-10 19:39:20 +0200

    test various sst methods

--- _bugscript/show_bug_in_docker.sh
+++ _bugscript/show_bug_in_docker.sh
@@ -1,18 +1,20 @@
-[ ! -z "$1" ] || exit 1
-
 tee Dockerfile <<EOF
 from centos:7
 
 RUN yum -y install m4 git wget python cmake make gcc-c++ ncurses-devel bison zlib zlib-devel zlib-static openssl vim findutils openssl vim m4 libaio libnuma numactl gnutls-devel openssl098e
 
-COPY ${1:example}.sh ${1:example}.sh
+RUN yum -y install http://www.percona.com/downloads/percona-release/redhat/0.1-4/percona-release-0.1-4.noarch.rpm
 
-RUN cat ${1:example}.sh
+RUN yum -y install percona-xtrabackup-24
+
+COPY ${1:-example}.sh ${1:-example}.sh
+
+RUN cat ${1:-example}.sh
 
 ENV WORKAROUND "$WORKAROUND"
 ENV WSREP_EXTRA_OPT "$WSREP_EXTRA_OPT"
 
-RUN bash -v -x ${1:example}.sh
+RUN bash -v -x ${1:-example}.sh
 EOF
 
 docker build .
