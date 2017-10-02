13861525e77afa088bedab86a765b413b807b3b8
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-10-02 12:51:59 +0200

    nicer wait of node start

--- m-system/wait_respond.sh
+++ m-system/wait_respond.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # galera needs to sleep some time to let node finish recovery
 # disable galera plugin if you don't need this sleep
-sleep 15
+sleep 5
 sudo mysqladmin --wait=5 ping
--- m-system2/wait_respond.sh
+++ m-system2/wait_respond.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # galera needs to sleep some time to let node finish recovery
 # disable galera plugin if you don't need this sleep
-sleep 15 
+
+# first wait X seconds til pid file is created
+counter=45
+if [ "$counter" -ge 0 ] && [ ! -e __workdir/dt/p.id ]
+  sleep 1
+  $((decr counter))
+fi
 mysqladmin --defaults-file=__workdir/my.cnf --wait=5 ping
