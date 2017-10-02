38527b293f8347a34ba0ed3a1b203cf35ee32326
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-10-02 11:33:53 +0200

    .

--- _template/cluster/galera_setup_acl.sh
+++ _template/cluster/galera_setup_acl.sh
@@ -16,6 +16,10 @@ for eid in $(cat __clusterdir/nodes.lst) ; do
   $eid*/sql.sh grant all on \*.\* to "$(whoami)"@localhost
   $eid*/sql.sh create user if not exists "galera"@"127.0.0.1" identified by '"galera"'
   $eid*/sql.sh grant all on \*.\* to galera@127.0.0.1
+  $eid*/sql.sh create user if not exists mysql@localhost
+  $eid*/sql.sh grant all on \*.\* to mysql@localhost
+  $eid*/sql.sh create user if not exists "mysql"@"127.0.0.1"
+  $eid*/sql.sh grant all on \*.\* to mysql@127.0.0.1  
   $eid*/shutdown.sh
 done
 
