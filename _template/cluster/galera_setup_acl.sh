00b8f56b11b99c920a89a4f492cca847285786e2
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-10-02 14:51:59 +0200

    testing 10.0

--- _template/cluster/galera_setup_acl.sh
+++ _template/cluster/galera_setup_acl.sh
@@ -10,17 +10,17 @@ firstnode=""
 for eid in $(cat __clusterdir/nodes.lst) ; do
   [ -z "$firstnode" ] || firstnode=$eid
   $eid*/startup.sh
-  $eid*/sql.sh create user if not exists "$(whoami)"@"$(__clusterdir/../$eid*/galera_ip.sh)"
+  $eid*/sql.sh create user /*M!100100 if not exists*/ "$(whoami)"@"$(__clusterdir/../$eid*/galera_ip.sh)"
   $eid*/sql.sh grant all on \*.\* to "$(whoami)"@"$(__clusterdir/../$eid*/galera_ip.sh)"
-  $eid*/sql.sh create user if not exists "$(whoami)"@localhost
+  $eid*/sql.sh create user /*M!100100 if not exists*/ "$(whoami)"@localhost
   $eid*/sql.sh grant all on \*.\* to "$(whoami)"@localhost
-  $eid*/sql.sh create user if not exists "galera"@"127.0.0.1" identified by '"galera"'
+  $eid*/sql.sh create user /*M!100100 if not exists*/ "galera"@"127.0.0.1" identified by '"galera"'
   $eid*/sql.sh grant all on \*.\* to galera@127.0.0.1
-  $eid*/sql.sh create user if not exists mysql@localhost
+  $eid*/sql.sh create user /*M!100100 if not exists*/ mysql@localhost
   $eid*/sql.sh grant all on \*.\* to mysql@localhost
-  $eid*/sql.sh create user if not exists "mysql"@"127.0.0.1"
+  $eid*/sql.sh create user /*M!100100 if not exists*/ "mysql"@"127.0.0.1"
   $eid*/sql.sh grant all on \*.\* to mysql@127.0.0.1  
-  $eid*/sql.sh create user if not exists "mysql"@"$(__clusterdir/../$eid*/galera_ip.sh)"
+  $eid*/sql.sh create user /*M!100100 if not exists*/ "mysql"@"$(__clusterdir/../$eid*/galera_ip.sh)"
   $eid*/sql.sh grant all on \*.\* to "mysql"@"$(__clusterdir/../$eid*/galera_ip.sh)"
   $eid*/shutdown.sh
 done
