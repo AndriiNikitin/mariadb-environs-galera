2a848f122afe5ccea8327b6f44c4d18037b345fd
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-11-09 04:33:31 +0100

    do not include port if default into address

--- m-all/galera_read_ip.sh
+++ m-all/galera_read_ip.sh
@@ -1,2 +1,7 @@
-echo $(__workdir/galera_ip.sh):$(__workdir/galera_port.sh)
+p="$(__workdir/galera_port.sh)"
+if [ "$p" == 4567 ]; then
+  echo $(__workdir/galera_ip.sh)
+else
+  echo $(__workdir/galera_ip.sh):$p
+fi
 
